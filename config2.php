0229ed4d2c057a71c806e8eb7fc9df0ab1a35003
Vozmen <105993180+Vozmen@users.noreply.github.com>
2022-11-11 17:38:50 +0300

    Create config2.php

---
+++ config2.php
@@ -0,0 +1,25 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocobz25kjlqq',
+  'passwordsalt' => 'xrU4zDwa93huR/7DgLq3EUW1g39hRp',
+  'secret' => 'fm7W/NiQy10ZZacquxHNUOjLd5OlG7Sn3KsmZhf712+uTbnE',
+  'trusted_domains' =>
+  array (
+    0 => '172.20.0.53',
+    1 => '172.20.0.48',
+    2 => 'cloud2.netnoir.ru',
+  ),
+  'datadirectory' => '/nextcloud',
+  'dbtype' => 'mysql',
+  'version' => '25.0.1.1',
+  'overwrite.cli.url' => 'http://172.20.0.53',
+  'dbname' => 'testdb',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'test',
+  'dbpassword' => 'qwe123',
+  'installed' => true,
+  'check_data_directory_permissions' => false,
+);
