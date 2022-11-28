ae447715fec27e8c3646989666fe7901048388b1
Vozmen <105993180+Vozmen@users.noreply.github.com>
2022-11-28 17:30:29 +0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -9,7 +9,9 @@ sudo sh -c "echo 127.0.1.1 nginx.netnoir.ru > /etc/hosts"
 sudo sh -c "echo 172.20.0.48 nginx >> /etc/hosts"
 #
 yum install epel-release -y
-yum install nginx -y
+mkdir /root/RPMs
+wget https://nginx.org/packages/mainline/centos/7/x86_64/RPMS/nginx-1.9.9-1.el7.ngx.x86_64.rpm -O /root/RPMs/nginx-1.9.9-1.el7.ngx.x86_64.rpm
+rpm -i /root/RPMs/nginx-1.9.9-1.el7.ngx.x86_64.rpm
 #
 systemctl disable --now firewalld
 
@@ -17,9 +19,8 @@ curl -s -X POST https://api.telegram.org/bot5920470511:AAHk7V77EaXhL64-0e7gpAqOW
 sleep 2
 
 #Filebeat installation
-mkdir /root/RPMs
 wget https://cloud.netnoir.ru/index.php/s/WTZwZZHiirZQoic/download/filebeat_7.17.3_x86_64-224190-4c3205.rpm -O /root/RPMs/filebeat_7.17.3_x86_64-224190-4c3205.rpm --no-check-certificate
-rpm -i /root/RPMs/*.rpm
+rpm -i /root/RPMs/filebeat_7.17.3_x86_64-224190-4c3205.rpm
 wget https://cloud.netnoir.ru/index.php/s/QKca4tmJ5SEBX9k/download/filebeat.yml -O /etc/filebeat/filebeat.yml --no-check-certificate
 
 curl -s -X POST https://api.telegram.org/bot5920470511:AAHk7V77EaXhL64-0e7gpAqOWgcNOiHDmoQ/sendMessage -d chat_id=191948484 -d text="BALANCER: FILEBEAT INSTALLATION COMPLETED"
