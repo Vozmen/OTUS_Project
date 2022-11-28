ffe12958c036e839e17d4c327c47a9cd35390746
Vozmen <105993180+Vozmen@users.noreply.github.com>
2022-11-28 17:36:12 +0300

    Update fin.ps1

--- fin.ps1
+++ fin.ps1
@@ -1,13 +1,7 @@
-ssh cloud1 "
-wget https://raw.githubusercontent.com/Vozmen/OTUS_Project/main/cloud1.sh -O /home/cloud1/cl1 /
-wget https://raw.githubusercontent.com/Vozmen/OTUS_Project/main/cloud2.sh -O /home/cloud1/cl2 /
-wget https://raw.githubusercontent.com/Vozmen/OTUS_Project/main/nginx.sh -O /home/cloud1/nginx /
-wget https://raw.githubusercontent.com/Vozmen/OTUS_Project/main/promet.sh -O /home/cloud1/prom /
-chmod +x /home/cloud1/cl1 /home/cloud1/cl2 /home/cloud1/nginx /home/cloud1/prom
-ssh cloud2@cloud2 'bash -s' < cl2 & /
-sleep 1 /
-ssh root@nginx 'bash -s' < nginx & /
-sleep 1 /
-ssh promet@promet 'bash -s' < prom & /
-sleep 1 /
-/home/cloud1/cl1 &"
+ssh nginx "yum install wget -y"
+ssh elk "yum install wget -y"
+
+$srv = 'web1', 'web2', 'promet', 'nginx', 'elk'
+$srv | foreach-Object -parallel{
+    ssh $_ "sudo wget https://raw.githubusercontent.com/Vozmen/OTUS_Project/main/$_.sh -O /root/script && sudo chmod +x /root/script && sudo /root/script"
+} &"
